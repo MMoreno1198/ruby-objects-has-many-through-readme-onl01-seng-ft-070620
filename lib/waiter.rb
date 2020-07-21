230583e3020cad0abf718a168a9d11455c9dde82
Marcus Moreno <legendary.1198@gmail.com>
2020-07-21 23:28:26 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -33,7 +33,7 @@ class Waiter
     
     Meal.all.select do |meal|
       
-      meal.waiter == self #checking for waiter now
+      meal.waiter == self 
       
     end
   end
@@ -41,12 +41,14 @@ class Waiter
 
   def best_tipper
     
-    
     best_tipped_meal = meals.max do |meal_a, meal_b|
+      
       meal_a.tip <=> meal_b.tip
       
     end
+    
   
     best_tipped_meal.customer
+    
   end
 end 
